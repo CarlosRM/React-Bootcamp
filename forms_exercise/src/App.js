eb8eec986dbb0636a8ef30db5020310639213cd3
Carlos <carlos.roldan@ludiumlab.com>
2020-08-13 19:24:58 +0200

    Forms exercise

---
+++ forms_exercise/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Box from './Box';
+import BoxList from './BoxList';
+import NewBoxForm from './NewBoxForm';
+
+function App() {
+  return (
+    <div>
+      <BoxList />
+    </div>
+  );
+}
+
+export default App;
